1ce973ad0ba4e3bf645c3495d1f7a9c57ad136c8
Taisei <taisei.mrchil@gmail.com>
2022-06-14 12:48:51 +0900

    csvをarrayに変更する関数を文理、dpi変更関数を追加。トリミング関数は未完成。

--- csv_to_img.py
+++ csv_to_img.py
@@ -2,23 +2,12 @@ import pandas as pd
 import numpy as np
 import csv
 import cv2
-import os
 from downsampling import downsampling
 
 
-def csv2img():
-  # filepath
-  FILE_PATH = 'C:/Users/Taisei/development/test/サインテスト２/Csv'
-  FILE_NAME = 'Sign-1-藤澤大世-1-True-1.csv'
-  OUTPUT_FILE_NAME = 'out.png'
-  # flame size
-  X_MAX = 13000
-  Y_MAX = 2200
-  # downsampling rate(minimum: 1)
-  DSR = 1
-
+def csv2nparray(csvpath, rate, x_max, y_max):
   # read CSV
-  with open(FILE_PATH + '/' + FILE_NAME, 'r') as f:
+  with open(csvpath, 'r') as f:
     reader = csv.reader(f)
     lines = [row for row in reader]
   # cut surplus
@@ -31,20 +20,72 @@ def csv2img():
   df.set_index("Time", inplace=True)
 
   # np.array all whose components are zero
-  arr = np.ones((X_MAX, Y_MAX))
+  arr = np.ones((x_max, y_max))
   # extract writing spot
   df_writing = df[df["Thickness"] != 0]
   # append writing spot
   for x, y in zip(df_writing["X cood."], df_writing["Y cood."]):
-    arr = downsampling(arr, x, y, DSR, X_MAX, Y_MAX)
+    arr = downsampling(arr, x, y, rate, x_max, y_max)
 
-  # after processing
-  arr *= 255
-  # make np.array image
-  cv2.imwrite(OUTPUT_FILE_NAME, arr.T)
+  return arr
+
+def changedpi(img, resolation):
+  h, w = img.shape
+  scale = (resolation / (w * h)) **0.5
+  changed_img = cv2.resize(img, dsize=None, fx=scale, fy=scale)
+
+  return changed_img
+
+def trimming(img):
+  up, down, left, right = -1, -1, -1, -1
+  for i, column in enumerate(img):
+    if np.all(column == 1) and up == -1:
+      pass
+    elif np.all(column == 1) and up != -1:
+      down = i
+    else:
+      if up == -1:
+        up = i
+      else:
+        continue
+      for j, element in enumerate(column):
+        if element == 1 and left == -1:
+          pass
+        elif element == 1 and left != -1:
+          right = j
+        elif left == -1:
+          left = j
+        else:
+          pass
+
+  return img[up:down, left:right]
 
 def main():
-  csv2img()
+  # filepath
+  #FILE_PATH = 'C:/Users/Taisei/development/test/サインテスト２/Csv'
+  #FILE_NAME = 'Sign-1-藤澤大世-1-True-1.csv'
+  FILE_PATH = './csv'
+  FILE_NAME = 'Sign-1-藤澤大世-1-True-1.csv'
+  OUTPUT_FILE_NAME = './out.png'
+  # flame size
+  X_MAX = 13000
+  Y_MAX = 2200
+  # downsampling rate(minimum: 1)
+  DSR = 10
+  # target dpi
+  RESOLATION = 640 ** 2
+
+  # convert csv into np.ndarray
+  arr = csv2nparray(FILE_PATH + '/' + FILE_NAME, DSR, X_MAX, Y_MAX)
+  arr_trimed = trimming(arr)
+
+  # postprocessing
+  arr *= 255
+
+  # make image out of np.ndarray
+  cv2.imwrite(OUTPUT_FILE_NAME, arr.T)
+  cv2.imwrite(OUTPUT_FILE_NAME + 'dpichanged.png', changedpi(arr, RESOLATION).T)
+  cv2.imwrite(OUTPUT_FILE_NAME + 'trimmed.png', changedpi(arr_trimed, RESOLATION).T)
 
 if __name__ == '__main__':
   main()
