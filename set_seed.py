966dd65971640909bb160ad35c06f5543487e3d7
Taisei <taisei.mrchil@gmail.com>
2022-08-15 02:53:59 +0900

    seedを設定

---
+++ set_seed.py
@@ -0,0 +1,13 @@
+def set_seed(seed=1):
+  from keras import backend as K
+  K.clear_session()
+  import os
+  os.environ['PYTHONHASHSEED'] = str(seed)
+  os.environ['TF_DETERMINISTIC_OPS'] = 'true'
+  os.environ['TF_CUDNN_DETERMINISTIC'] = 'true'
+  import tensorflow as tf
+  import numpy as np
+  import random
+  tf.random.set_seed(seed)
+  np.random.seed(seed)
+  random.seed(seed)
