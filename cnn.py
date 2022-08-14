c8d2694487749baa39749740d6a892fbfa0b5bd6
Taisei <taisei.mrchil@gmail.com>
2022-08-15 02:24:25 +0900

    軽微な修整

--- cnn.py
+++ cnn.py
@@ -41,3 +41,24 @@ def cifar10_cnn(cnf):
   model.compile(optimizer=cnf.optimizer, loss=cnf.lossfunc, metrics=['accuracy'])
 
   return model
+
+def mnist_cnn(cnf):
+  color = (1,) if cnf.color == 'grayscale' else (3,)
+  model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=cnf.input_shape + color),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    tf.keras.layers.Dropout(0.25),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', name=cnf.last_layername),
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    tf.keras.layers.Dropout(0.25),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(cnf.outputs, activation='softmax')
+  ], name=cnf.model_name)
+  model.compile(optimizer=cnf.optimizer, loss=cnf.lossfunc, metrics=['accuracy'])
+
+  return model
